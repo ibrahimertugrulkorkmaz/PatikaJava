cc90e66996c4ae881769f2aa21d5ee0821770cf0
İbrahim Ertuğrul Korkmaz <ibrahimertugrulkorkmaz@gmail.com>
2022-03-30 14:32:22 +0300

    Add files via upload

--- IdeaProjects/PatikaJava/src/FibonacciNumber.java
+++ IdeaProjects/PatikaJava/src/FibonacciNumber.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class FibonacciNumber {
     public static void main(String[] args) {
-        int boyut,sayi1=0,sayi2=0,sonuc;
+        int boyut;
         Scanner input = new Scanner(System.in);
         System.out.print("Fibonacci serisi için gireceğiniz boyut: ");
         boyut= input.nextInt();
+        int sayi1=0;
+        int sayi2=1;
+        int sonuc;
         sonuc=sayi1+sayi2;
         System.out.print("0 1 1");
         for(int i=0;i<boyut;i++) {
