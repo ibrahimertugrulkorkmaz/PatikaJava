02f5023414780a8d24b405b214cd2bf21b7fe9a0
İbrahim Ertuğrul Korkmaz <ibrahimertugrulkorkmaz@gmail.com>
2022-04-03 20:02:01 +0300

    Student Information System

---
+++ IdeaProjects/PatikaJava/src/studentInformationSystem/StudentInformationInput.java
@@ -0,0 +1,31 @@
+package studentInformationSystem;
+
+public class StudentInformationInput {
+    public static void main(String[] args) {
+
+        Course mat = new Course("Matematik", "MAT101", "MAT");
+        Course fizik = new Course("Fizik", "FZK101", "FZK");
+        Course kimya = new Course("Kimya", "KMY101", "KMY");
+
+        Teacher t1 = new Teacher("Sevil Yıldırım", "90550000000", "MAT");
+        Teacher t2 = new Teacher("Haluk Bilginer", "90550000001", "FZK");
+        Teacher t3 = new Teacher("Refik Soylu", "90550000002", "KMY");
+
+        mat.addTeacher(t1);
+        fizik.addTeacher(t2);
+        kimya.addTeacher(t3);
+
+        Student s1 = new Student("Seda Balaban", 4, "140144015", mat, fizik, kimya);
+        s1.addBulkExamNote(50, 20, 40);
+        s1.isPass();
+
+        Student s2 = new Student("Andre Silva", 4, "2211133", mat, fizik, kimya);
+        s2.addBulkExamNote(100, 50, 40);
+        s2.isPass();
+
+        Student s3 = new Student("Sevil Soyer", 4, "221121312", mat, fizik, kimya);
+        s3.addBulkExamNote(50, 20, 40);
+        s3.isPass();
+
+    }
+}
